2700457bd7eeadf9ff596a91c96b31bb71668cc3
feigebaobei <18515195415@163.com>
2018-10-21 16:41:11 +0800

    confirm

--- src/components/test/iview.vue
+++ src/components/test/iview.vue
@@ -2,24 +2,46 @@
   <div class="basic">
     <p>v-model={{value}}</p>
     <first v-model="value"></first>
+    <hr>
+    <button @click="openConf">open confirm</button>
+    <confirm v-model="confirmShow" @onCancel="cancel" @onOk="ok">
+      <p>info info info info info info</p>
+    </confirm>
+    <hr>
   </div>
 </template>
 
 <script>
 import first from './first.vue'
+import confirm from '@/components/common/confirm.vue'
 export default {
   props: [''],
   data () {
     return {
-      value: 0
+      value: 0,
+      confirmShow: false
     }
   },
   computed: {
   },
   components: {
-    first
+    first,
+    confirm
   },
-  methods: {},
+  methods: {
+    // open confirm
+    openConf: function () {
+      //
+      this.confirmShow = true
+    },
+    //
+    cancel: function () {
+      console.log('cancel')
+    },
+    ok: function () {
+      console.log('ok')
+    }
+  },
   mounted () {}
 }
 </script>
